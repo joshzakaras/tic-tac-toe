9b0992262c8632a6fc3545ad1e0c2d17dbb88fbe
Josh Zakaras <joshzakaras@gmail.com>
2024-10-22 21:45:14 -0700

    removed gui elements from incorrect branch

--- spec/tic_tac_toe/quil_ui/quil_ui_spec.clj
+++
@@ -1,6 +0,0 @@
-(ns tic-tac-toe.quil-ui.quil-ui-spec
-  (:require [speclj.core :refer :all]))
-
-
-(describe "A quil based Tic Tac Toe GUI"
-  (it ""))
--- src/tic_tac_toe/quil_ui/quil_ui.clj
+++
@@ -1,2 +0,0 @@
-(ns tic-tac-toe.quil-ui.quil-ui
-  (:require [quil.core :as quil]))
